7b959532a9bf9f854cbffeb34e27940c612e94e7
martaclj <104948192+martaclj@users.noreply.github.com>
2025-03-05 20:55:00 +0100

    routes y ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductoViewComponent } from './pages/producto-view/producto-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'producto/:_id', component: ProductoViewComponent },
+  { path: '**', redirectTo: 'home' }
+];
---
+++ src/app/interfaces/iproduct.ts
@@ -0,0 +1,9 @@
+export interface Iproduct {
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  image: string;
+  active: boolean;
+}
